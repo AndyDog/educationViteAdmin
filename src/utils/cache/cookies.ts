b6f790ecd6dbfedbabc4e956f72a6818e945a0bd
梁海军 <1090631724@qq.com>
2024-05-09 22:46:00 +0800

    0000000

--- src/utils/cache/cookies.ts
+++ src/utils/cache/cookies.ts
@@ -7,7 +7,7 @@ export const getToken = () => {
   return Cookies.get(CacheKey.TOKEN)
 }
 export const setToken = (token: string) => {
-  Cookies.set(CacheKey.TOKEN, token)
+  Cookies.set(CacheKey.TOKEN, token,{ expires: 7 })
 }
 export const removeToken = () => {
   Cookies.remove(CacheKey.TOKEN)
--- src/views/system/role-manage/index.vue
+++ src/views/system/role-manage/index.vue
@@ -13,8 +13,8 @@ import {
   addDictionaiesDetail
 } from "@/api/table"
 
-import { usergetRole, usergetRoleUser, useraddRole, userdeleteRole, userupdateRole } from "@/api/user"
-
+import { usergetRole, usergetRoleUser, useraddRole, userdeleteRole, userupdateRole,useraddUser,userupdateUser,userdeleteUser,usergetByUserLike } from "@/api/user"
+// import { useraddUser, usergetByUserLike, userdeleteUser, userupdateUser, usergetRole } from "@/api/user"
 import { type IGetTableData } from "@/api/table/types/table"
 import { type FormInstance, type FormRules, ElMessage, ElMessageBox } from "element-plus"
 import { ElTable } from "element-plus"
@@ -39,7 +39,7 @@ const {
 
 const dialogVisible = ref<boolean>(false)
 const dialogVisibleDetail = ref<boolean>(false)
-
+let imageUrl = ref<any>("")
 const formRef = ref<FormInstance | null>(null)
 const formRefDetail = ref<FormInstance | null>(null)
 const formData = reactive<any>({
@@ -50,7 +50,6 @@ const formData = reactive<any>({
   // dictRemark: "", //备注
   // insertTime: "", //新增时间
   // updateTime: "" //修改时间
-
   remark: "", //备注
   roleCode: "", //角色代码
   roleId: 0, //角色ID
@@ -78,9 +77,9 @@ const formDataDetail = reactive<any>({
 })
 
 const formRules: FormRules = reactive({
-  dicNumber: [{ required: true, trigger: "blur", message: "请输入排序号" }],
-  dictCode: [{ required: true, trigger: "blur", message: "请输入字典值" }],
-  dictName: [{ required: true, trigger: "blur", message: "字典名称" }]
+  roleName: [{ required: true, trigger: "blur", message: "请输入角色名称" }],
+  roleCode: [{ required: true, trigger: "blur", message: "请输入角色代码" }],
+  sort: [{ required: true, trigger: "blur", message: "排序号" }]
 })
 const formRulesDetail: FormRules = reactive({
   loginName: [{ required: true, trigger: "blur", message: "请输入账号" }],
@@ -95,6 +94,8 @@ const setCurrent = (row?: any) => {
 }
 const handleCurrentChangeTable = (val: any | undefined) => {
   currentRow.value = val
+        formDataDetail.userRoleList = []
+      formDataDetail.userRoleList.push(currentRow.value.roleId) 
   getTableDataDetail()
 }
 
@@ -125,12 +126,12 @@ const handleCreate = () => {
 const handleCreateDetail = () => {
   formRef.value?.validate((valid: boolean) => {
     if (valid) {
-      let param = JSON.parse(JSON.stringify(formData))
+      let param = JSON.parse(JSON.stringify(formDataDetail))
       //
       param.userRoleList = param.userRoleList.map((item) => {
         let obj = {}
-        obj = optionsType.value.filter((itemm) => {
-          return itemm.roleCode == item
+        obj = tableData.value.filter((itemm) => {
+          return itemm.roleId == item
         })[0]
         return obj
       })
@@ -138,14 +139,14 @@ const handleCreateDetail = () => {
       if (currentUpdateId.value === undefined) {
         useraddUser(param).then(() => {
           ElMessage.success("新增成功")
-          dialogVisible.value = false
-          getTableData()
+          dialogVisibleDetail.value = false
+          getTableDataDetail()
         })
       } else {
         userupdateUser(formData).then(() => {
           ElMessage.success("修改成功")
-          dialogVisible.value = false
-          getTableData()
+          dialogVisibleDetail.value = false
+          getTableDataDetail()
         })
       }
     } else {
@@ -280,6 +281,8 @@ const getTableData = () => {
       paginationData.total = res?.datas?.length
       tableData.value = res?.datas
       setCurrent(res?.datas?.[0])
+      formDataDetail.userRoleList = []
+      formDataDetail.userRoleList.push(res?.datas?.[0].roleId) 
       getTableDataDetail()
     })
     .catch(() => {
@@ -343,6 +346,33 @@ const handleRefreshDetail = () => {
   getTableDataDetail()
 }
 
+
+const adduser=()=>{
+  dialogVisibleDetail.value = true
+}
+const handleAvatarSuccess = (res: any, file: any) => {
+  imageUrl.value = URL.createObjectURL(file.raw)
+  formData.imagePath = res.data
+  console.log(imageUrl)
+}
+// const getTableDataDetailRole = () => {
+//   usergetRole({
+//     page: 1,
+//     size: 100000
+//     // parentCode: "course_classification",
+//     // type: 1
+//     // username: searchData.username || undefined,
+//     // phone: searchData.phone || undefined
+//   })
+//     .then((res: any) => {
+//       console.log(res)
+//       optionsType.value = res?.datas
+//     })
+//     .catch(() => {
+//       optionsType.value = []
+//     })
+// }
+
 //#endregion
 
 /** 监听分页参数的变化 */
@@ -383,7 +413,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
                 @current-change="handleCurrentChangeTable"
               >
                 <!-- <el-table-column type="index" width="50" align="center" /> -->
-                <el-table-column prop="roleName" label="字典值" align="center" />
+                <el-table-column prop="roleName" label="" align="center" />
                 <!-- <el-table-column prop="dictName" label="字典名称" align="center">
                   <template #default="scope">
                     {{ scope.row.dictName }}
@@ -407,22 +437,27 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
           <!-- 新增/修改 -->
           <el-dialog
             v-model="dialogVisible"
-            :title="currentUpdateId === undefined ? '添加字典' : '修改字典'"
+            :title="currentUpdateId === undefined ? '添加' : '修改'"
             @close="resetForm"
             width="900px"
           >
             <el-form ref="formRef" :model="formData" :rules="formRules" label-width="100px" label-position="left">
-              <el-form-item prop="dictName" label="字典名称">
-                <el-input v-model="formData.dictName" placeholder="请输入" />
+              <!-- <el-form-item prop="roleId" label="上级角色">
+                <el-select v-model="formData.roleId" placeholder="上级角色" clearable>
+                  <el-option v-for="item in tableData" :label="item.roleName" :value="item.roleId" />
+                </el-select>
+              </el-form-item> -->
+              <el-form-item prop="roleName" label="角色名称" v-if="currentUpdateId === undefined">
+                <el-input v-model="formData.roleName" placeholder="请输入" />
               </el-form-item>
-              <el-form-item prop="dictCode" label="字典值" v-if="currentUpdateId === undefined">
-                <el-input v-model="formData.dictCode" placeholder="请输入" />
+              <el-form-item prop="roleCode" label="角色代码">
+                <el-input v-model="formData.roleCode" placeholder="请输入" />
               </el-form-item>
-              <el-form-item prop="dicNumber" label="排序号">
-                <el-input-number v-model="formData.dicNumber" :min="1" :max="1000" label="排序号"></el-input-number>
+              <el-form-item prop="sort" label="排序号">
+                <el-input v-model="formData.sort" placeholder="请输入" />
               </el-form-item>
-              <el-form-item prop="dictRemark" label="备注">
-                <el-input type="textarea" :rows="3" placeholder="请输入内容" v-model="formData.dictRemark"> </el-input>
+              <el-form-item prop="emark" label="备注">
+                <el-input type="textarea" :rows="3" placeholder="请输入内容" v-model="formData.emark"> </el-input>
               </el-form-item>
             </el-form>
             <template #footer>
@@ -447,22 +482,22 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
             </el-form>
           </el-card> -->
 
-          <el-card v-loading="loading" shadow="never" class="search-wrapper">
-            <el-form ref="searchFormRef" :inline="true" :model="searchData">
+          <el-card  shadow="never" class="search-wrapper">
+            <el-form ref="searchFormRef" :inline="true" :model="searchDataDetail">
               <el-form-item prop="loginName" label="账号">
-                <el-input v-model="searchData.loginName" placeholder="请输入" />
+                <el-input v-model="searchDataDetail.loginName" placeholder="请输入" />
               </el-form-item>
               <!-- <el-form-item prop="phone" label="用户类型">
                 <el-input v-model="searchData.phone" placeholder="请输入" />
               </el-form-item> -->
               <el-form-item prop="userName" label="姓名">
-                <el-input v-model="searchData.userName" placeholder="请输入" />
+                <el-input v-model="searchDataDetail.userName" placeholder="请输入" />
               </el-form-item>
               <el-form-item prop="phone" label="手机号">
-                <el-input v-model="searchData.phone" placeholder="请输入" />
+                <el-input v-model="searchDataDetail.phone" placeholder="请输入" />
               </el-form-item>
               <el-form-item prop="phone" label="性别">
-                <el-input v-model="searchData.phone" placeholder="请输入" />
+                <el-input v-model="searchDataDetail.phone" placeholder="请输入" />
               </el-form-item>
               <!-- <el-form-item prop="phone" label="出生年月">
                 <el-input v-model="searchData.phone" placeholder="请输入" />
@@ -471,7 +506,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
                 <el-input v-model="searchData.phone" placeholder="请输入" />
               </el-form-item> -->
               <el-form-item prop=" idCard" label="身份证号">
-                <el-input v-model="searchData.idCard" placeholder="请输入" />
+                <el-input v-model="searchDataDetail.idCard" placeholder="请输入" />
               </el-form-item>
               <!-- <el-form-item prop="phone" label="机构">
                 <el-input v-model="searchData.phone" placeholder="请输入" />
@@ -483,11 +518,13 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
             </el-form>
           </el-card>
 
-          <el-card v-loading="loading" shadow="never">
+          <el-card v-loading="loadingDetail" shadow="never">
             <div class="toolbar-wrapper">
               <div>
-                <el-button type="primary" :icon="CirclePlus" @click="dialogVisible = true">新增用户</el-button>
-                <el-button type="danger" :icon="Delete">批量删除</el-button>
+                <!-- {{dialogVisibleDetail}} -->
+                <!-- dialogVisibleDetail = true -->
+                <el-button type="primary" :icon="CirclePlus" @click="adduser">新增</el-button>
+                <el-button type="danger" :icon="Delete">删除</el-button>
               </div>
               <div>
                 <el-tooltip content="下载">
@@ -499,7 +536,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
               </div>
             </div>
             <div class="table-wrapper">
-              <el-table :data="tableData">
+              <el-table :data="tableDataDetail">
                 <el-table-column type="selection" width="50" align="center" />
                 <el-table-column prop=" loginName" label="账号" align="center" />
                 <el-table-column prop="phone" label="手机号" align="center" />
@@ -543,13 +580,13 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
             <div class="pager-wrapper">
               <el-pagination
                 background
-                :layout="paginationData.layout"
-                :page-sizes="paginationData.pageSizes"
-                :total="paginationData.total"
-                :page-size="paginationData.pageSize"
-                :currentPage="paginationData.currentPage"
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
+                :layout="paginationData1.layout"
+                :page-sizes="paginationData1.pageSizes"
+                :total="paginationData1.total"
+                :page-size="paginationData1.pageSize"
+                :currentPage="paginationData1.currentPage"
+                @size-change="handleSizeChange1"
+                @current-change="handleCurrentChange1"
               />
             </div>
           </el-card>
@@ -557,20 +594,20 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
           <!-- 新增/修改 -->
           <!-- 新增/修改 -->
           <el-dialog
-            v-model="dialogVisible"
-            :title="currentUpdateId === undefined ? '用户信息' : '修改'"
+            v-model="dialogVisibleDetail"
+            :title="currentUpdateIdDetail === undefined ? '新增用户' : '修改'"
             @close="resetForm"
             width="900px"
           >
-            <el-form ref="formRef" :model="formData" :rules="formRules" label-width="100px" label-position="left">
+            <el-form ref="formRef" :model="formDataDetail" :rules="formRulesDetail" label-width="100px" label-position="left">
               <el-row :gutter="20">
                 <el-col :span="12">
                   <el-form-item prop="loginName" label="账号">
-                    <el-input v-model="formData.loginName" placeholder="请输入" /> </el-form-item
+                    <el-input v-model="formDataDetail.loginName" placeholder="请输入" /> </el-form-item
                 ></el-col>
                 <el-col :span="12">
                   <el-form-item prop="userName" label="姓名">
-                    <el-input v-model="formData.userName" placeholder="请输入" /> </el-form-item
+                    <el-input v-model="formDataDetail.userName" placeholder="请输入" /> </el-form-item
                 ></el-col>
                 <!-- <el-col :span="12">
                   <el-form-item prop="name" label="用户类型" v-if="currentUpdateId === undefined">
@@ -580,18 +617,18 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
               <el-row :gutter="20">
                 <el-col :span="12">
                   <el-form-item prop="phone" label="手机号" v-if="currentUpdateId === undefined">
-                    <el-input v-model="formData.phone" placeholder="请输入" /> </el-form-item
+                    <el-input v-model="formDataDetail.phone" placeholder="请输入" /> </el-form-item
                 ></el-col>
 
                 <el-col :span="12">
                   <el-form-item prop="password" label="登录密码" v-if="currentUpdateId === undefined">
-                    <el-input v-model="formData.password" placeholder="请输入" /> </el-form-item
+                    <el-input v-model="formDataDetail.password" placeholder="请输入" /> </el-form-item
                 ></el-col>
               </el-row>
               <el-row :gutter="20">
                 <el-col :span="12">
                   <el-form-item prop="gender" label="性别">
-                    <el-select v-model="formData.gender" placeholder="性别" clearable>
+                    <el-select v-model="formDataDetail.gender" placeholder="性别" clearable>
                       <el-option label="男" value="1" />
                       <el-option label="女" value="2" />
                     </el-select>
@@ -601,7 +638,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
                   <el-form-item prop="birthday" label="出生日期">
                     <!-- <el-input v-model="formData.password" placeholder="请输入" />  -->
                     <el-date-picker
-                      v-model="formData.birthday"
+                      v-model="formDataDetail.birthday"
                       type="date"
                       placeholder="出生日期"
                       format="YYYY/MM/DD"
@@ -612,13 +649,13 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
               </el-row>
               <el-row :gutter="20">
                 <el-col :span="12">
-                  <el-form-item prop="userRoleList" label="角色">
-                    <el-select v-model="formData.userRoleList" placeholder="角色" multiple clearable>
+                  <el-form-item prop="userRoleList" label="角色" v-if="tableData">
+                    <el-select v-model="formDataDetail.userRoleList" placeholder="角色" multiple clearable>
                       <el-option
-                        v-for="item in optionsType"
-                        :key="item.roleCode"
+                        v-for="item in tableData"
+                        :key="item.roleId"
                         :label="item.roleName"
-                        :value="item.roleCode"
+                        :value="item.roleId"
                       >
                       </el-option>
                     </el-select>
@@ -633,7 +670,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
                       start-placeholder="考试开始时间"
                       end-placeholder="考试结束时间"
                     /> -->
-                    <el-input v-model="formData.idCard" placeholder="请输入" />
+                    <el-input v-model="formDataDetail.idCard" placeholder="请输入" />
                   </el-form-item>
                 </el-col>
               </el-row>
@@ -657,7 +694,7 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
                   <el-form-item label="用户头像:" prop="imagePath">
                     <!-- //pic为了验证图片是必传的 -->
                     <el-input
-                      v-model="formData.imagePath"
+                      v-model="formDataDetail.imagePath"
                       style="height: 0px; width: 0px; visibility: hidden"
                     ></el-input>
                     <!-- <upload-img @updateFileList="updateFiles"></upload-img> -->
@@ -682,12 +719,12 @@ watch([() => paginationData1.currentPage, () => paginationData1.pageSize], getTa
               </el-row>
 
               <el-form-item label="个人简介">
-                <el-input v-model="formData.intro" type="textarea" />
+                <el-input v-model="formDataDetail.intro" type="textarea" />
               </el-form-item>
             </el-form>
             <template #footer>
-              <el-button @click="dialogVisible = false">取消</el-button>
-              <el-button type="primary" @click="handleCreate">确认</el-button>
+              <el-button @click="dialogVisibleDetail = false">取消</el-button>
+              <el-button type="primary" @click="handleCreateDetail">确认</el-button>
             </template>
           </el-dialog>
         </div>
